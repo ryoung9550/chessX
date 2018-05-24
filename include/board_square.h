6cf1f260204f2bd129c41ae6b1b1e2e22ee6144a
ryoung <ryoung@giga-debian.uc.rcyoung.net>
2018-05-23 23:34:43 -0400

    Added implementation for GameBoard

--- include/board_square.h
+++ include/board_square.h
@@ -7,12 +7,10 @@
 
 class BoardSquare
 {
-	Pos squarePos;
 	std::unique_ptr<GamePiece> currentPiece;
 public:
 	BoardSquare();
-	BoardSquare(Pos& initPos);
-	BoardSquare(Pos& initPos, GamePiece *const piece);
+	BoardSquare(GamePiece *const piece);
 	std::unique_ptr<GamePiece> removePiece();
 	void setPiece(std::unique_ptr<GamePiece>);
 };
