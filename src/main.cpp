3a71500c7109ecdb4c19f63def1f9073864a58c1
ryoung <ryoung@giga-debian.uc.rcyoung.net>
2018-06-23 09:55:12 -0400

    changes to window creation and CMakeLists

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
+#include <SDL2/SDL.h>
 #include <iostream>
+
+#include "media/window.h"
 
 int main()
 {
 	std::cout << "chicken shit\n";
+	Window win("chicken shit lul");
+	for(;;);
 
 	return 0;
 }
--- src/media/window.cpp
+++ src/media/window.cpp
@@ -1,4 +1,5 @@
 #include "media/window.h"
+#include "media/SDLInit.h"
 #include "common/globals.h"
 #include "exceptions/SDL_exceptions.h"
 
@@ -7,12 +8,11 @@
 #include <SDL2/SDL.h>
 
 
-Window::Window(std::string title) :
-	window(SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED, globals::DEFAULT_WIN_H, 
-		globals::DEFAULT_WIN_W, 0)),
-	renderer(SDL_CreateRenderer(window.get(), -1, 0))
+Window::Window(std::string title)
 {
+	window = std::make_unique(SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED, globals::DEFAULT_WIN_H, 
+		globals::DEFAULT_WIN_W, 0));
 	if (this->window.get() == nullptr) {
 		throw new window_init_failed_exception();
 	}
