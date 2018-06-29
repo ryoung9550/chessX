3de9e9f651a2ddc8c5fe30c4947cb58663be0977
ryoung <ryoung@giga-debian.uc.rcyoung.net>
2018-06-28 23:08:10 -0400

    Added documentation of View methods

--- include/media/view.h
+++ include/media/view.h
@@ -11,7 +11,7 @@
 
 class View
 {
-	SDL_Rect pos;
+	SDL_Rect pos; // Dimensions and position of View
 	std::unique_ptr<SDL_Renderer, SDL_Deleter> renderer;
 	std::vector<DrawObject> renderList;
 	std::unordered_map<char*, std::shared_ptr<SDL_Texture>> textures;
@@ -25,9 +25,38 @@ class View
 	 */
 	bool inView(DrawObject);
 public:
+	/*
+	 * Renders all Draw Object which are visible to the View in
+	 * order of farthest objects to nearest.
+	 */
 	void draw();
-	void loadTexture(std::string);
+
+	/*
+	 * Loads a PNG or JPG image from the filepath provided. Creates
+	 * the texture object then adds it to the textures Map.
+	 * @param std::string - Name used to access Texture from getTexture()
+	 * @param std::string - Path of PNG or JPG image
+	 *
+	 * @see getTexture()
+	 * @throws logic_error - If Name already exists in the Map
+	 */
+	void loadTexture(std::string, std::string);
+
+	/*
+	 * Removes the Texture reference from the Map. The Texture
+	 * will remain to exist until all references from the Draw
+	 * Objects have also been cleaned up.
+	 * @param std::string - Name of texture to be removed from the Map
+	 */
 	void unloadTexture(std::string);
+
+	/*
+	 * Gets a reference to a texture with the Name used to add
+	 * the texture previously from loading it.
+	 * @param std::string - Name of Texture indexed into the Map
+	 *
+	 * @throws out_of_range - If texture is not indexed
+	 */
 	std::shared_ptr<SDL_Texture> getTexture(std::string);
 };
 
